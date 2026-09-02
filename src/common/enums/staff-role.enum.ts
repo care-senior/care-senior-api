67e4519c988a13b24875c4a697c6998f6cff5399
vinicius.flores <vinicius.flores@wooba.com.br>
2026-09-02 14:28:16 -0300

    fix: use english enum keys for StaffRole

--- src/common/enums/staff-role.enum.ts
+++ src/common/enums/staff-role.enum.ts
@@ -1,11 +1,11 @@
 export enum StaffRole {
-  COORDENADORA = 'Coordenadora',
-  ENFERMEIRA = 'Enfermeira',
-  CUIDADOR = 'Cuidador',
+  COORDINATOR = 'Coordenadora',
+  NURSE = 'Enfermeira',
+  CAREGIVER = 'Cuidador',
 }
 
 // Coordenadora/Enfermeira aprovam ou recusam solicitações (vínculo, saída) e desvinculam idosos.
 export const ROLES_THAT_MANAGE_REQUESTS = [
-  StaffRole.COORDENADORA,
-  StaffRole.ENFERMEIRA,
+  StaffRole.COORDINATOR,
+  StaffRole.NURSE,
 ];
